81aef55e302de6db772f24a46b5bb122792316ad
CARTOON01 <brianngugi224@gmail.com>
2023-08-25 18:09:54 +0300

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
