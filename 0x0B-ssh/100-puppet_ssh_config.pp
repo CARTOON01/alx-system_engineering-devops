66740537b03dd5087a86469a206042e2fb5e10b2
Brian Kamau <brian@briankamau.com>
2025-07-05 19:02:43 +0300

    Add Puppet script to configure SSH client with private key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,41 @@
-#configure ssh config
+# 100-puppet_ssh_config.pp
 
-file_line{'Turn off passwd auth':
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentication no'
+file { '/home/cartoon/.ssh':
+  ensure => directory,
+  owner  => 'cartoon',
+  group  => 'cartoon',
+  mode   => '0700',
 }
 
-file_line{'Declare identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school'
+file { '/home/cartoon/.ssh/config':
+  ensure  => file,
+  owner   => 'cartoon',
+  group   => 'cartoon',
+  mode    => '0600',
+  content => '',
+  require => File['/home/cartoon/.ssh'],
+}
 
+file_line { 'Set host':
+  path  => '/home/cartoon/.ssh/config',
+  line  => 'Host school-server',
+  match => '^Host ',
+}
+
+file_line { 'Set user':
+  path  => '/home/cartoon/.ssh/config',
+  line  => '    User ubuntu',
+  match => '^ *User',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/cartoon/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^ *IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/cartoon/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^ *PasswordAuthentication',
 }
